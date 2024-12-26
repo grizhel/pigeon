ab98759920d9219ac8a90ce8888459521acc9543
grizhla <emreyilmazin@gmail.com>
2024-12-26 15:12:18 +0300

    From NUnit -> XUnit

--- pigeon-unit-test/ContactTest.cs
+++ pigeon-unit-test/ContactTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Reflection.Metadata;
+using Microsoft.AspNetCore.Builder.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Moq;
@@ -10,63 +11,30 @@ using pigeon_crud_service.Services;
 using pigeon_crud_service.Utils;
 using pigeon_lib.Interfaces.ServiceInterfaces;
 using pigeon_lib.Models.Interfaces.ModelInterfaces;
+using Xunit;
 
 namespace pigeon_unit_test
 {
-	public class ContactTest
+	public class ContactTest : TestBase
 	{
-		public dynamic PrepareEnvironment()
+		private readonly ContactService contactService;
+
+		public ContactTest(IOptions<IAppOptions> options)
 		{
-			var dbContext = new Mock<PigeonDBContext>(); 
-			AppOptions _appOptions = new() { ListLimit = 8 };
-			var contactService = new Mock<ContactService>(dbContext, new Mock<IOptions<AppOptions>>(_appOptions));
-
-			var data = Scenario.CreateDbScenario();
-
-
-			var mockSetContact = new Mock<DbSet<Contact>>();
-			mockSetContact.As<IQueryable<Contact>>().Setup(m => m.GetEnumerator()).Returns(() => data.contacts.GetEnumerator());
-
-			var mockSetFirm = new Mock<DbSet<Firm>>();
-			mockSetFirm.As<IQueryable<Firm>>().Setup(m => m.GetEnumerator()).Returns(() => data.firms.GetEnumerator());
-
-			var mockSetLocation = new Mock<DbSet<Location>>();
-			mockSetLocation.As<IQueryable<Location>>().Setup(m => m.GetEnumerator()).Returns(() => data.locations.GetEnumerator());
-
-
-			dbContext.Setup(m => m.Contacts).Returns(mockSetContact.Object);
-			dbContext.Setup(m => m.Firms).Returns(mockSetFirm.Object);
-			dbContext.Setup(m => m.Locations).Returns(mockSetLocation.Object);
-
-			var _contactService = contactService.Object;
-			var _dbContext = dbContext.Object;
-
-			return new { service = _contactService, dbContext = _dbContext, data };
-		}
-
-		public dynamic GetEnvironmet()
-		{
-			var preps = PrepareEnvironment();
-			var service = (IService<Contact>)preps.service;
-			var dbContext = (PigeonDBContext)preps.dbContext;
-			var data = preps.data;
-			var contactData = (List<Contact>)data.contacts;
-			var firmData = (List<Firm>)data.firms;
-			var locationData = (List<Location>)data.locations;
-			var contactInfoData = (List<ContactInfo>)data.contactInfos;
-			return new { service, dbContext, contactData, firmData, locationData, contactInfoData };
+			options.Value.ListLimit = 8;
+			contactService = new(dbContext, options);
 		}
 
 
-		[TestCase]
+		[Fact]
 		public void GetTest()
 		{
-			var env = GetEnvironmet();
-			var passWithId = env.contactData.First().Id;
-			Assert.That(env.service.Get(passWithId) != null);
+			var datum = data.contacts.First();
+			Assert.True(contactService.Get(datum.Id) != null);
+			Assert.Equals(contactService.Get(datum.Id), datum);
 
 			var failWithId = Guid.NewGuid();
-			Assert.That(env.service.Get(failWithId) == null);
+			Assert.True(contactService.Get(failWithId) == null);
 		}
 	}
 }
---
+++ pigeon-unit-test/TestBase.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Storage;
+using Moq;
+using Npgsql;
+using pigeon_crud_service.Models;
+using System;
+
+
+namespace pigeon_unit_test
+{
+	public class TestBase : IDisposable
+	{
+
+		protected readonly PigeonDBContext dbContext;
+		protected readonly dynamic data;
+		private bool disposedValue;
+
+		protected TestBase()
+		{
+			data = Scenario.CreateDbScenario();
+			dbContext = PrepareDatabase("EF-InMemory");
+		}
+
+
+		private PigeonDBContext PrepareDatabase(string databaseType)
+		{
+			DbContextOptions options;
+			PigeonDBContext pigeonDBContext;
+			switch (databaseType)
+			{
+				case "EF-InMemory":
+					options = new DbContextOptionsBuilder<PigeonDBContext>()
+							.ConfigureWarnings(x => x.Ignore(CoreEventId.ManyServiceProvidersCreatedWarning))
+							.UseInMemoryDatabase("PigeonDBContext")
+							.Options;
+					pigeonDBContext = new PigeonDBContext(options, null);
+
+					pigeonDBContext.Database.EnsureDeleted();
+					pigeonDBContext.Database.EnsureCreated();
+
+					
+					pigeonDBContext.Contacts.Add(data.contacts);
+					pigeonDBContext.Firms.Add(data.firms);
+					pigeonDBContext.Locations.Add(data.locations);
+					pigeonDBContext.ContactInformations.Add(data.contactInformations);
+					pigeonDBContext.SaveChanges();
+
+					break;
+				default:
+					throw new Exception();
+			}
+
+			return pigeonDBContext;
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposedValue)
+			{
+				if (disposing)
+				{
+					// TODO: dispose managed state (managed objects)
+				}
+
+				// TODO: free unmanaged resources (unmanaged objects) and override finalizer
+				// TODO: set large fields to null
+				disposedValue = true;
+			}
+		}
+
+		// // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+		// ~TemelTest()
+		// {
+		//     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+		//     Dispose(disposing: false);
+		// }
+
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+			Dispose(disposing: true);
+			GC.SuppressFinalize(this);
+		}
+	}
+}
+
